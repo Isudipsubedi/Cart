73a5c49c1bde9d138629c0800da063e0bed8ec3f
Sudip <subedisudipyt@gmail.com>
2022-11-10 18:41:01 +0545

    Added

---
+++ src/components/product.js
@@ -0,0 +1,43 @@
+export const products = [
+    {
+      id: 1,
+      title: "Leather Jacket",
+      description: "black in color",
+      price: "400",
+      img: "https://th.bing.com/th/id/OIP.LIzSsPL0cAdxCzXm1N8ZTwHaEK?pid=ImgDet&rs=1",
+      quantity: 1,
+    },
+    {
+      id: 2,
+      title: "Lehenga",
+      description: "white in color",
+      price: "700",
+      img: "https://th.bing.com/th/id/OIP.MQsliBjEbJxfKN36MEfebwHaHa?pid=ImgDet&rs=1",
+      quantity: 1,
+    },
+    {
+      id: 3,
+      title: "Cultural dress",
+      description: "gurung",
+      price: "200",
+      img: "https://th.bing.com/th/id/R.9060bd51a653b29902927073b78a41af?rik=0YjOiumnlyJXow&pid=ImgRaw&r=0",
+      quantity: 1,
+    },
+    {
+      id: 4,
+      title: "Sari",
+      description: "Bridal Sari",
+      price: "500",
+      img: "https://3.bp.blogspot.com/-xXkdPOD5Tfc/WyRLSLAKNoI/AAAAAAAAA1k/LXIvsmuiHOoPoj7Wr_ywjfdGcejiFQ_uwCLcBGAs/s1600/9027_1n.jpg",
+      quantity: 1,
+    },
+    {
+      id: 5,
+      title: "Newar Cultural dress",
+      description: "Bridal Sari",
+      price: "300",
+      img: "https://th.bing.com/th/id/OIP.uWC2Mu0_4k4lUxtIP5yxTgAAAA?pid=ImgDet&rs=1",
+      quantity: 1,
+    },
+   
+  ];
